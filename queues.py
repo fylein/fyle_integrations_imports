31e5b069967c21777c5477e1775a4dc9ff82f9c0
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-03-04 22:04:05 +0530

    Added custom_properties field in task_setting and modified trigger fn

--- queues.py
+++ queues.py
@@ -9,6 +9,12 @@ def chain_import_fields_to_fyle(workspace_id, task_settings: TaskSetting):
     """
     chain = Chain()
 
+    if task_settings['custom_properties'] is not None:
+        chain.append(
+            task_settings['custom_properties']['func'],
+            **task_settings['custom_properties']['args']
+        )
+
     if task_settings['import_categories']:
         chain.append(
             'fyle_integrations_imports.tasks.trigger_import_via_schedule',
