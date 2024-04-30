42f74a8472e19784d0894549a5846d69837315d2
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-04-30 20:29:47 +0530

    fix chart_of_account none error

--- modules/categories.py
+++ modules/categories.py
@@ -72,7 +72,7 @@ class Category(Base):
         if attribute_type != 'CATEGORY':
             if 'accounts' in self.destination_sync_methods:
                 account_filters = filters & Q(display_name='Account')
-                if len(self.charts_of_accounts) > 0:
+                if hasattr(self, 'charts_of_accounts') and len(self.charts_of_accounts) > 0:
                     account_filters &= Q(detail__account_type__in=self.charts_of_accounts)
 
             if 'items' in self.destination_sync_methods:
