e1e3cb837b5e6e8aefccd90be50c981f41f2ddae
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2024-09-05 15:15:43 +0530

    fix: disable categories if import is enabled and check code import conditionally (#30)

--- modules/categories.py
+++ modules/categories.py
@@ -248,7 +248,7 @@ class Category(Base):
         return mapped_attribute_ids
 
 
-def disable_categories(workspace_id: int, categories_to_disable: Dict, **kwargs):
+def disable_categories(workspace_id: int, categories_to_disable: Dict, is_import_to_fyle_enabled: bool = False, *args, **kwargs):
     """
     categories_to_disable object format:
     {
@@ -260,6 +260,10 @@ def disable_categories(workspace_id: int, categories_to_disable: Dict, **kwargs)
         }
     }
     """
+    if not is_import_to_fyle_enabled or len(categories_to_disable) == 0:
+        logger.info("Skipping disabling categories in Fyle | WORKSPACE_ID: %s", workspace_id)
+        return
+
     fyle_credentials = FyleCredential.objects.get(workspace_id=workspace_id)
     platform = PlatformConnector(fyle_credentials=fyle_credentials)
 
@@ -270,6 +274,11 @@ def disable_categories(workspace_id: int, categories_to_disable: Dict, **kwargs)
 
     category_values = []
     for category_map in categories_to_disable.values():
+        if not use_code_in_naming and category_map['value'] == category_map['updated_value']:
+            continue
+        elif use_code_in_naming and (category_map['value'] == category_map['updated_value'] and category_map['code'] == category_map['update_code']):
+            continue
+
         category_name = import_string('apps.mappings.helpers.prepend_code_to_name')(prepend_code_in_name=use_code_in_naming, value=category_map['value'], code=category_map['code'])
         category_values.append(category_name)
 
@@ -281,10 +290,10 @@ def disable_categories(workspace_id: int, categories_to_disable: Dict, **kwargs)
     }
 
     expense_attribute_value_map = {}
-    for k, v in categories_to_disable.items():
+    for destination_id, v in categories_to_disable.items():
         category_name = v['value']
         category_name = import_string('apps.mappings.helpers.prepend_code_to_name')(prepend_code_in_name=use_code_in_naming, value=v['value'], code=v['code'])
-        expense_attribute_value_map[category_name] = k
+        expense_attribute_value_map[category_name] = destination_id
 
     bulk_payload = []
 
